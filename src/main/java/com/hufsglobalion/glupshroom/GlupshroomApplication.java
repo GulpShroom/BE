d6c76b7efd12175a3c948f90f5cfa4c91ccb346d
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-08-01 19:25:36 +0900

    chore: 백엔드 환경 설정 완료

---
+++ src/main/java/com/hufsglobalion/glupshroom/GlupshroomApplication.java
@@ -0,0 +1,13 @@
+package com.hufsglobalion.glupshroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlupshroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlupshroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hufsglobalion/glupshroom/GlupshroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufsglobalion.glupshroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlupshroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
