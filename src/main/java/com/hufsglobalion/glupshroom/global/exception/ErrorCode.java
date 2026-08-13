f81d82bdf0f93e82ad04eae642f110429c3765e9
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-08-13 11:45:49 +0900

    feature: 에러 코드 정의

---
+++ src/main/java/com/hufsglobalion/glupshroom/global/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.hufsglobalion.glupshroom.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    UNSUPPORTED_PROFILE_TYPE(HttpStatus.BAD_REQUEST, "E400", "지원하지 않는 프로필 타입입니다"),
+    PROFILE_SELECTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "E500", "프로필 선택 중 오류가 발생했습니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E500", "서버 내부 오류가 발생했습니다");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
