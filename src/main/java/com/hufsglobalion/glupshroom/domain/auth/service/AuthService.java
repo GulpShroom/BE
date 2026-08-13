c1d15f146661bb1f054afd270d287601131ed019
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-08-13 11:51:49 +0900

    feature: 프로필 선택 서비스 구현

---
+++ src/main/java/com/hufsglobalion/glupshroom/domain/auth/service/AuthService.java
@@ -0,0 +1,26 @@
+package com.hufsglobalion.glupshroom.domain.auth.service;
+
+import com.hufsglobalion.glupshroom.domain.auth.dto.response.ProfileSelectResponse;
+import com.hufsglobalion.glupshroom.domain.user.entity.ProfileType;
+import com.hufsglobalion.glupshroom.domain.user.entity.User;
+import com.hufsglobalion.glupshroom.domain.user.repository.UserRepository;
+import com.hufsglobalion.glupshroom.global.exception.CustomException;
+import com.hufsglobalion.glupshroom.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public ProfileSelectResponse selectProfile(String profileTypeValue) {
+        ProfileType profileType = ProfileType.from(profileTypeValue);
+        User user = userRepository.findByProfileType(profileType)
+                .orElseThrow(() -> new CustomException(ErrorCode.PROFILE_SELECTION_FAILED));
+        return ProfileSelectResponse.from(user);
+    }
+}
