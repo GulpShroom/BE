521b66538e551a2fef5abf2693dbf50cdb764a66
kangnyy <rkdskdus216@gmail.com>
2026-08-14 22:44:41 +0900

    feature: JourneyRepository 구현

---
+++ src/main/java/com/hufsglobalion/glupshroom/domain/journey/repository/JourneyRepository.java
@@ -0,0 +1,9 @@
+package com.hufsglobalion.glupshroom.domain.journey.repository;
+
+import com.hufsglobalion.glupshroom.domain.journey.entity.Journey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JourneyRepository extends JpaRepository<Journey, Long> {
+
+    long countByProductIdAndAuthorId(Long productId, Long authorId);
+}
