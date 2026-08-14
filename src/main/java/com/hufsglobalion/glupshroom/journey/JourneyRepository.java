18f2cd5f088b7742072071fee6da456c0c9201c1
wonnbu <pcwlove0510@gmail.com>
2026-08-15 02:54:20 +0900

    feature: JourneyRepository 작성

---
+++ src/main/java/com/hufsglobalion/glupshroom/journey/JourneyRepository.java
@@ -0,0 +1,6 @@
+package com.hufsglobalion.glupshroom.journey;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JourneyRepository extends JpaRepository<Journey, Long> {
+}
