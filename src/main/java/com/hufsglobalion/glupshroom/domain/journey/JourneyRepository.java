d7e59b9118ed215dfc0f980290874688dc905a5f
wonnbu <pcwlove0510@gmail.com>
2026-08-15 18:55:31 +0900

    refactor: Journey 엔티티/Repository domain 구조로 이동 및 필드 재설계

--- src/main/java/com/hufsglobalion/glupshroom/domain/journey/JourneyRepository.java
+++ src/main/java/com/hufsglobalion/glupshroom/domain/journey/JourneyRepository.java
@@ -1,6 +1,7 @@
-package com.hufsglobalion.glupshroom.journey;
+package com.hufsglobalion.glupshroom.domain.journey;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JourneyRepository extends JpaRepository<Journey, Long> {
+    long countByProductId(Long productId);
 }
