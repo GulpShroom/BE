a6bc524fe7a9baa48ff172605494cb93894253c3
kangnyy <rkdskdus216@gmail.com>
2026-08-17 20:48:27 +0900

    feature: 제품 스캔 정품 확인 API 컨트롤러 구현

--- src/main/java/com/hufsglobalion/glupshroom/domain/product/controller/ProductController.java
+++ src/main/java/com/hufsglobalion/glupshroom/domain/product/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.hufsglobalion.glupshroom.domain.product.controller;
 
+import com.hufsglobalion.glupshroom.domain.product.dto.request.ProductScanRequest;
 import com.hufsglobalion.glupshroom.domain.product.dto.response.MyProductListResponse;
 import com.hufsglobalion.glupshroom.domain.product.dto.response.ProductLineageResponse;
+import com.hufsglobalion.glupshroom.domain.product.dto.response.ProductScanResponse;
 import com.hufsglobalion.glupshroom.domain.product.dto.response.ProductSummaryResponse;
 import com.hufsglobalion.glupshroom.domain.product.service.ProductService;
 import com.hufsglobalion.glupshroom.global.common.ApiResponse;
@@ -10,6 +12,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +41,13 @@ public class ProductController {
     public ApiResponse<ProductSummaryResponse> getProductSummary(@PathVariable Long productId) {
         ProductSummaryResponse response = productService.getProductSummary(productId);
         return ApiResponse.success("제품 요약 정보를 조회했습니다", response);
+    }
+
+    @Operation(summary = "제품 스캔·정품 확인", description = "시리얼 번호 또는 QR 코드로 제품 정품 여부를 확인합니다.")
+    @PostMapping("/products/scan")
+    public ApiResponse<ProductScanResponse> scanProduct(@RequestBody ProductScanRequest request) {
+        ProductScanResponse response = productService.scanProduct(request);
+        return ApiResponse.success("MCM 제품 인증이 완료되었습니다", response);
     }
 
     @Operation(summary = "계보 타임라인 조회", description = "제품의 세대별 소유 이력을 계보 타임라인으로 조회합니다.")
