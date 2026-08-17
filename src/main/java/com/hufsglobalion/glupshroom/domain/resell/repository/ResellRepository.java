94cdd5f11d238995efcea4e2f53a9cc3a5b1fc46
KimJuHui <juhui4078@hufs.ac.kr>
2026-08-17 17:10:54 +0900

    [BE-#9] 계승 시작 API 구현 (#17)
    * feat: Resell 엔티티 작성
    
    * fix: Transfer 엔티티에 ID 자동 생성 및 생성자 추가
    
    * feat: Resell 엔티티에 판매중 상태 확인 메서드 추가
    
    * feat: ResellRepository 추가
    
    * feat: 제품별 진행 중 이전 존재 여부 조회 쿼리 추가
    
    * feat: 계승 시작 요청 DTO 추가
    
    * feat: 계승 시작 응답 DTO 추가
    
    * feat: 계승 시작(리셀 구매) 서비스 로직 구현
    
    * feat: 계승 시작 API 엔드포인트 추가
    
    * feat: 계승 시작 API 에러코드 추가
    
    ---------
    
    Co-authored-by: 김주희 <juhui@gimjuhuiui-MacBookPro.local>

---
+++ src/main/java/com/hufsglobalion/glupshroom/domain/resell/repository/ResellRepository.java
@@ -0,0 +1,7 @@
+package com.hufsglobalion.glupshroom.domain.resell.repository;
+
+import com.hufsglobalion.glupshroom.domain.resell.entity.Resell;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResellRepository extends JpaRepository<Resell, Long> {
+}
