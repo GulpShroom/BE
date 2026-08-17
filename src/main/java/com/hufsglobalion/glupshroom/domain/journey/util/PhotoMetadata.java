45414c8f3e47bb4b38a783592628ddd06070a795
wonnbu <pcwlove0510@gmail.com>
2026-08-17 22:56:41 +0900

    feat: PhotoMetadata에 takenAt 필드 추가

--- src/main/java/com/hufsglobalion/glupshroom/domain/journey/util/PhotoMetadata.java
+++ src/main/java/com/hufsglobalion/glupshroom/domain/journey/util/PhotoMetadata.java
@@ -1,9 +1,12 @@
 package com.hufsglobalion.glupshroom.domain.journey.util;
 
+import java.time.LocalDateTime;
+
 public record PhotoMetadata(
         Integer year,
         Integer month,
         String season,
         String country,
-        String city
+        String city,
+        LocalDateTime takenAt
 ) {}
