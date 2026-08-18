81335d5e5f0091f63784a73a985f3a6f676d2e06
kangnyy <rkdskdus216@gmail.com>
2026-08-19 00:19:24 +0900

    feature: 온 디스 데이 조회 예외 코드 추가

--- src/main/java/com/hufsglobalion/glupshroom/global/exception/ErrorCode.java
+++ src/main/java/com/hufsglobalion/glupshroom/global/exception/ErrorCode.java
@@ -25,6 +25,8 @@ public enum ErrorCode {
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "E404", "제품을 찾을 수 없습니다"),
     PRODUCT_OWNER_MISMATCH(HttpStatus.FORBIDDEN, "E403", "해당 제품의 현재 소유자만 여정을 등록할 수 있습니다"),
     JOURNEY_NOT_FOUND(HttpStatus.NOT_FOUND, "E404", "여정을 찾을 수 없습니다"),
+    ON_THIS_DAY_USER_ID_REQUIRED(HttpStatus.BAD_REQUEST, "E400", "사용자 ID가 필요합니다"),
+    ON_THIS_DAY_RETRIEVAL_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "E500", "온 디스 데이 조회 중 오류가 발생했습니다"),
 
     INVALID_PRODUCT_LIST_STATUS(HttpStatus.BAD_REQUEST, "E400", "소유 상태 조회 조건이 올바르지 않습니다"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "E404", "사용자를 찾을 수 없습니다"),
