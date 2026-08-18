9a044bcf39ff800b0b8e551ef65745acc67165c5
wonnbu <pcwlove0510@gmail.com>
2026-08-18 16:42:08 +0900

    feat: ResellRepository에 목록 조회 쿼리 메서드 추가

--- src/main/java/com/hufsglobalion/glupshroom/domain/resell/repository/ResellRepository.java
+++ src/main/java/com/hufsglobalion/glupshroom/domain/resell/repository/ResellRepository.java
@@ -1,7 +1,15 @@
 package com.hufsglobalion.glupshroom.domain.resell.repository;
 
 import com.hufsglobalion.glupshroom.domain.resell.entity.Resell;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResellRepository extends JpaRepository<Resell, Long> {
+
+    Page<Resell> findByPostStatus(String postStatus, Pageable pageable);
+
+    Page<Resell> findByPostStatusAndSellerId(String postStatus, Long sellerId, Pageable pageable);
+
+    Page<Resell> findByPostStatusAndBuyerId(String postStatus, Long buyerId, Pageable pageable);
 }
